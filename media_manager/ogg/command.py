aa7443e50ff233e50d44b23d4d9e800cd0c67f48
Stephen Czetty <stephen@houseofczetty.com>
2020-12-07 16:04:45 -0600

    Add in code to add album art to an ogg file.
    Copied almost verbatim from the rough script I used before.  Needs
    cleanup.

--- media_manager/ogg/command.py
+++ media_manager/ogg/command.py
@@ -1,5 +1,11 @@
 import argparse
 
+import sys
+import base64
+from mutagen.oggopus import OggOpus
+from mutagen.flac import Picture
+from PIL import Image
+
 def create_parser(subparsers: argparse._SubParsersAction):
     parser = subparsers.add_parser('ogg')
 
@@ -11,3 +17,28 @@ def create_parser(subparsers: argparse._SubParsersAction):
 
 def add_image(args: argparse.Namespace):
     print(f'Adding an image from {args.image_file.name} to {args.ogg_file.name}')
+
+    audio = OggOpus(args.ogg_file)
+    im = Image.open(args.image_file)
+    width, height = im.size
+    fmt = im.format
+    print(fmt)
+    with open(args.image_file.name, "rb") as c:
+        data = c.read()
+    picture = Picture()
+    picture.data = data
+    picture.type = 17
+    picture.desc = u"Cover Art"
+    picture.mime = u"image/jpeg"
+    picture.width = width
+    picture.height = height
+    picture.depth = 24
+    picture_data = picture.write()
+    encoded_data = base64.b64encode(picture_data)
+
+    vcomment_value = encoded_data.decode("ascii")
+    audio["metadata_block_picture"] = [vcomment_value]
+
+    audio.save()
+
+
