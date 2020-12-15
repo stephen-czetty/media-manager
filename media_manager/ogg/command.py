463a28c7bf9a891d3a4f59a1d0b04991b5a38fa7
Stephen Czetty <stephen@houseofczetty.com>
2020-12-15 15:37:54 -0600

    mp4 coverimage support.
    Needs to be merged into a single pathway.

--- media_manager/ogg/command.py
+++ media_manager/ogg/command.py
@@ -6,10 +6,12 @@ from mutagen import File
 from mutagen.oggopus import OggOpus
 from mutagen.oggvorbis import OggVorbis
 from mutagen.flac import Picture
+from mutagen.mp4 import MP4, MP4Cover
 from PIL import Image
 
 _image_mimes = {
-    'JPEG': u'image/jpeg'
+    'JPEG': u'image/jpeg',
+    'PNG': u'image/png'
 }
 
 def create_parser(subparsers: argparse._SubParsersAction):
@@ -25,7 +27,16 @@ def create_parser(subparsers: argparse._SubParsersAction):
     tag_ogg_parser.add_argument('ogg_file', help='Input ogg file', type=str)
     tag_ogg_parser.add_argument('--artist', help='Set artist', type=str)
     tag_ogg_parser.add_argument('--title', help='Set title', type=str)
+    tag_ogg_parser.add_argument('--genre', help='Set genre (can be specified multiple times)', type=str, action='append')
     tag_ogg_parser.set_defaults(func=tag_ogg)
+
+    # TODO: Combine this with above and remove the ogg/mp4 portion of the command.
+    parser = subparsers.add_parser('mp4')
+    mp4parsers = parser.add_subparsers(required=True, help='MP4 help', dest='mp4command')
+    cover_image_parser = mp4parsers.add_parser('coverimage', aliases=['cover', 'image'])
+    cover_image_parser.add_argument('mp4_file', help='Input mp4 file', type=str)
+    cover_image_parser.add_argument('image_file', help='Cover image', type=str)
+    cover_image_parser.set_defaults(func=add_mp4_image)
 
 def _open_ogg(file_name):
     return File(file_name, options=[OggOpus, OggVorbis])
@@ -62,5 +73,26 @@ def tag_ogg(args: argparse.Namespace):
         audio['ARTIST'] = args.artist
     if args.title:
         audio['TITLE'] = args.title
+    if args.genre:
+        audio['GENRE'] = args.genre[0]
+        for next_genre in args.genre[1:]:
+            audio.tags.append(('GENRE', next_genre))
 
+    audio.save()
+
+_mp4_formats = {
+    'JPEG': MP4Cover.FORMAT_JPEG,
+    'PNG': MP4Cover.FORMAT_PNG
+}
+
+def add_mp4_image(args: argparse.Namespace):
+    audio = MP4(args.mp4_file)
+    with open(args.image_file, "rb") as fd:
+        image_data = fd.read()
+
+    im = Image.open(args.image_file)
+    image_format = _mp4_formats.get(im.format, MP4Cover.FORMAT_JPEG)
+    covr = [MP4Cover(image_data, image_format)]
+
+    audio.tags['covr'] = covr
     audio.save()
