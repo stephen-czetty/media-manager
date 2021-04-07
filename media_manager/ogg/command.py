4918cdabbc35842c5d70b59604299d98c82865b4
Stephen Czetty <stephen@houseofczetty.com>
2021-04-07 02:41:42 -0500

    Remove "mp4" option, coverfile is a top-level command now.

--- media_manager/ogg/command.py
+++ media_manager/ogg/command.py
@@ -1,5 +1,6 @@
 import argparse
 
+import io
 import sys
 import base64
 from mutagen import File
@@ -18,10 +19,6 @@ def create_parser(subparsers: argparse._SubParsersAction):
     parser = subparsers.add_parser('ogg')
 
     oggparsers = parser.add_subparsers(required=True, help='Ogg/vorbis help', dest='oggcommand')
-    cover_image_parser = oggparsers.add_parser('coverimage', aliases=['cover', 'image'])
-    cover_image_parser.add_argument('ogg_file', help='Input ogg file', type=str)
-    cover_image_parser.add_argument('image_file', help='Cover image', type=str)
-    cover_image_parser.set_defaults(func=add_image)
 
     tag_ogg_parser = oggparsers.add_parser('tag')
     tag_ogg_parser.add_argument('ogg_file', help='Input ogg file', type=str)
@@ -30,34 +27,22 @@ def create_parser(subparsers: argparse._SubParsersAction):
     tag_ogg_parser.add_argument('--genre', help='Set genre (can be specified multiple times)', type=str, action='append')
     tag_ogg_parser.set_defaults(func=tag_ogg)
 
-    # TODO: Combine this with above and remove the ogg/mp4 portion of the command.
-    parser = subparsers.add_parser('mp4')
-    mp4parsers = parser.add_subparsers(required=True, help='MP4 help', dest='mp4command')
-    cover_image_parser = mp4parsers.add_parser('coverimage', aliases=['cover', 'image'])
-    cover_image_parser.add_argument('mp4_file', help='Input mp4 file', type=str)
+    cover_image_parser = subparsers.add_parser('coverimage', aliases=['cover', 'image'])
+    cover_image_parser.add_argument('audio_file', help='Input audio file', type=str)
     cover_image_parser.add_argument('image_file', help='Cover image', type=str)
-    cover_image_parser.set_defaults(func=add_mp4_image)
+    cover_image_parser.set_defaults(func=add_image)
 
-def _open_ogg(file_name):
-    return File(file_name, options=[OggOpus, OggVorbis])
-
-def add_image(args: argparse.Namespace):
-    print(f'Adding an image from {args.image_file} to {args.ogg_file}')
-
-    audio = _open_ogg(args.ogg_file)
-    im = Image.open(args.image_file)
-    width, height = im.size
-    fmt = im.format
-
-    with open(args.image_file, "rb") as c:
-        data = c.read()
+def _open_audio(file_name):
+    return File(file_name, options=[OggOpus, OggVorbis, MP4])
+    
+def _add_ogg_image(audio: File, image: Image, image_data: bytes):
     picture = Picture()
-    picture.data = data
+    picture.data = image_data
     picture.type = 3 # Front cover
     picture.desc = u"Cover Art"
-    picture.mime = _image_mimes[fmt]
-    picture.width = width
-    picture.height = height
+    picture.mime = _image_mimes[image.format]
+    picture.width = image.width
+    picture.height = image.height
     picture.depth = 24
     picture_data = picture.write()
     encoded_data = base64.b64encode(picture_data)
@@ -68,7 +53,7 @@ def add_image(args: argparse.Namespace):
     audio.save()
 
 def tag_ogg(args: argparse.Namespace):
-    audio = _open_ogg(args.ogg_file)
+    audio = _open_audio(args.ogg_file)
     if args.artist:
         audio['ARTIST'] = args.artist
     if args.title:
@@ -85,14 +70,26 @@ _mp4_formats = {
     'PNG': MP4Cover.FORMAT_PNG
 }
 
-def add_mp4_image(args: argparse.Namespace):
-    audio = MP4(args.mp4_file)
-    with open(args.image_file, "rb") as fd:
-        image_data = fd.read()
-
-    im = Image.open(args.image_file)
-    image_format = _mp4_formats.get(im.format, MP4Cover.FORMAT_JPEG)
+def _add_mp4_image(audio: File, image: Image, image_data):
+    image_format = _mp4_formats.get(image.format, MP4Cover.FORMAT_JPEG)
     covr = [MP4Cover(image_data, image_format)]
 
     audio.tags['covr'] = covr
     audio.save()
+
+_coverimage_methods = {
+    OggOpus: _add_ogg_image,
+    OggVorbis: _add_ogg_image,
+    MP4: _add_mp4_image
+}
+
+def add_image(args: argparse.Namespace):
+    print(f'Adding an image from {args.image_file} to {args.audio_file}')
+
+    audio = _open_audio(args.audio_file)
+
+    im = Image.open(args.image_file)
+
+    with open(args.image_file, "rb") as c:
+        data = c.read()
+    _coverimage_methods[audio.__class__](audio, im, data)
